264bc88aca65121a5eff1ccdf6f604a02dc2ed38
Yogesh Shah <yogeshshah2063@gmail.com>
2024-12-09 17:52:30 +0545

    Add pagination

--- controllers/lyricsController.js
+++ controllers/lyricsController.js
@@ -1,13 +1,48 @@
 const Lyrics = require("../models/lyrics.models");
 
 const getAllLyrics = async (req, res) => {
-  const myData = await Lyrics.find(req.query);
+  const { songTitle, artist, album, genre, sort, select } = req.query;
 
-  res.status(200).json({ myData });
+  const queryObject = {};
+  if (songTitle) {
+    queryObject.songTitle = { $regex: songTitle, $options: "i" };
+  }
+
+  if (artist) {
+    queryObject.artist = { $regex: artist, $options: "i" };
+  }
+
+  if (album) {
+    queryObject.album = { $regex: album, $options: "i" };
+  }
+
+  if (genre) {
+    queryObject.genre = { $regex: genre, $options: "i" };
+  }
+
+  let apiResult = Lyrics.find(queryObject);
+  if (sort) {
+    const sortFix = sort.split(",").join(" ");
+    apiResult = apiResult.sort(sortFix);
+  }
+
+  if (select) {
+    const selectFix = select.split(",").join(" ");
+    apiResult = apiResult.select(selectFix);
+  }
+
+  let page = Number(req.query.page) || 1;
+  let limit = Number(req.query.limit) || 3;
+  let skip = (page - 1) * limit;
+  apiResult = apiResult.skip(skip).limit(limit);
+
+  const myLyrics = await apiResult;
+
+  res.status(200).json({ myLyrics });
 };
 
 const getAllLyricsTesting = async (req, res) => {
-  const { artist, songTitle, album, genre, language, sort } = req.query;
+  const { artist, songTitle, album, genre, language, sort, select } = req.query;
   const queryObject = {};
 
   if (artist) {
@@ -33,13 +68,24 @@ const getAllLyricsTesting = async (req, res) => {
   let apiResult = Lyrics.find(queryObject);
 
   if (sort) {
-    let sortFix = sort.replace(",", " ");
+    let sortFix = sort.split(",").join(" ");
     apiResult = apiResult.sort(sortFix);
   }
 
-  const myData = await apiResult;
+  if (select) {
+    let selectFix = select.split(",").join(" ");
+    apiResult = apiResult.select(selectFix);
+  }
 
-  res.status(200).json({ myData });
+  let page = Number(req.query.page);
+  let limit = Number(req.query.limit);
+  let skip = (page - 1) * limit;
+
+  apiResult = apiResult.skip(skip).limit(limit);
+
+  const myLyrics = await apiResult;
+
+  res.status(200).json({ myLyrics });
 };
 
 module.exports = { getAllLyrics, getAllLyricsTesting };
